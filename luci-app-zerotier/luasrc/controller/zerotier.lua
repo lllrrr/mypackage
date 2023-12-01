25042306eb740958fc310ca2b51b19bec5856211
lllrrr3 <5143158@qq.cm>
2023-12-02 00:44:10 +0800

    mod zerotier

--- luci-app-zerotier/luasrc/controller/zerotier.lua
+++ luci-app-zerotier/luasrc/controller/zerotier.lua
@@ -6,14 +6,14 @@ function index()
   end
   entry({"admin", "vpn"}, firstchild(), "VPN", 45).dependent = false
   entry({"admin", "vpn", "zerotier"}, alias("admin", "vpn", "zerotier", "settings"), _("ZeroTier"), 99)
-  entry({"admin", "vpn", "zerotier", "settings"},cbi("zerotier/settings"), _("Base Setting"), 1)
-  entry({"admin", "vpn", "zerotier", "info"},form("zerotier/info"), _("Interface Info"), 2)
-  entry({"admin", "vpn", "zerotier", "status"},call("act_status"))
+  entry({"admin", "vpn", "zerotier", "settings"}, cbi("zerotier/settings"), _("Base Setting"), 1)
+  entry({"admin", "vpn", "zerotier", "info"}, form("zerotier/info"), _("Interface Info"), 2)
+  entry({"admin", "vpn", "zerotier", "status"}, call("act_status"))
 end
 
 function act_status()
   local e={}
-  e.running=luci.sys.call("pgrep /usr/bin/zerotier-one >/dev/null") == 0
+  e.running = luci.sys.call("pgrep /usr/bin/zerotier-one >/dev/null") == 0
   luci.http.prepare_content("application/json")
   luci.http.write_json(e)
 end
